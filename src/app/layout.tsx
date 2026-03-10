b1ab9f7bb43785cfcedebe6749862f4f6699b9ac
Vasyl Yefimenko <rednez@gmail.com>
2026-03-10 18:44:16 +0100

    feat: integrate Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google';
 import { Navbar, ThemeProvider } from './components/layout';
@@ -38,6 +39,7 @@ export default function RootLayout({
       <body
         className={`${playfairDisplay.variable} ${geistMono.variable} ${geistSans.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-G9N8T0T8YR" />
         <ThemeProvider>
           <Navbar />
           {children}
